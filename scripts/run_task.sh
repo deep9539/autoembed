ec360bdf4b852a659949d1aa65027047cab123f4
deep9539 <deepshah.96@gmail.com>
2026-08-16 14:56:36 -0700

    Update run_task.sh

--- scripts/run_task.sh
+++ scripts/run_task.sh
@@ -357,6 +357,8 @@ start_vllm() {
     export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
     export TORCHDYNAMO_DISABLE=1
     export VLLM_USE_MODERN_TORCH_COMPILE=0
+    export VLLM_USE_AOT_COMPILE=0
+    export VLLM_DISABLE_COMPILE_CACHE=1
     export TRITON_CACHE_DIR="/tmp/triton-${SLURM_JOB_ID:-local}"
     mkdir -p "$TRITON_CACHE_DIR" && chmod 1777 "$TRITON_CACHE_DIR"
     # Since Qwen3.6-27B needs to be fetched, set HF_HUB_OFFLINE=0 to allow online downloading
